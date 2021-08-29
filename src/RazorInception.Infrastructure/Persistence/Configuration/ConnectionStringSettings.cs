a2c5430c3dec3f9db0984abb802255c48ca731d1
Alex Watts <alexjwatts83@gmail.com>
2021-08-30 08:35:10 +1000

    Added connections for db setup

---
+++ src/RazorInception.Infrastructure/Persistence/Configuration/ConnectionStringSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorInception.Infrastructure.Persistence.Configuration
+{
+	public class ConnectionStringSettings
+	{
+		public const string Section = "ConnectionStrings";
+		public string Database { get; set; }
+		public string Redis { get; set; }
+	}
+}
