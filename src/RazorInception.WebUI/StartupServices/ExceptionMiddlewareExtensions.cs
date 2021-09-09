e18d021b48ce274917f18c56084409ea089ec3c9
Alex Watts <alexjwatts83@gmail.com>
2021-09-10 08:10:01 +1000

    Small code styling clean up

--- src/RazorInception.WebUI/StartupServices/ExceptionMiddlewareExtensions.cs
+++ src/RazorInception.WebUI/StartupServices/ExceptionMiddlewareExtensions.cs
@@ -23,17 +23,16 @@ namespace RazorInception.WebUI.StartupServices
 					if (contextFeature != null)
 					{
 						var response = env.IsDevelopment()
-							? new ApiException((int)HttpStatusCode.InternalServerError, contextFeature.Error.Message, contextFeature.Error.StackTrace?.ToString())
+							? new ApiException(
+								(int)HttpStatusCode.InternalServerError,
+								contextFeature.Error.Message,
+								contextFeature.Error.StackTrace?.ToString())
 							: new ApiException((int)HttpStatusCode.InternalServerError);
 						var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 						var json = JsonSerializer.Serialize(response, options);
 
 						logger.LogError($"Something went wrong", contextFeature.Error);
-						//await context.Response.WriteAsync(new ErrorDetails()
-						//{
-						//	StatusCode = context.Response.StatusCode,
-						//	Message = "Internal Server Error."
-						//}.ToString());
+
 						await context.Response.WriteAsync(json).ConfigureAwait(false);
 					}
 				});
