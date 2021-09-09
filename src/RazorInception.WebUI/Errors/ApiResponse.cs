0a4ca9448a645a5fb9bbb6428bc11798c2c84f1b
Alex Watts <alexjwatts83@gmail.com>
2021-09-10 07:50:22 +1000

    Added api responses

---
+++ src/RazorInception.WebUI/Errors/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RazorInception.WebUI
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public DateTime Occured { get; } = DateTime.UtcNow;
+
+        private static string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorized, you are not",
+                404 => "Resource found, it was not",
+                405 => "Not allowed son",
+                500 => "Errors are the path to the dark side.  Errors lead to anger.   Anger leads to hate.  Hate leads to career change.",
+                _ => null
+            };
+        }
+    }
+}
---
+++ src/RazorInception.WebUI/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RazorInception.WebUI
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse() : base(400)
+        {
+            // TODO
+        }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
